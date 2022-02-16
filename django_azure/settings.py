d948e7c97b78397028adc9394c339337836a60a3
Bytegra <75863419+Bytegra@users.noreply.github.com>
2022-02-16 09:30:02 +0100

    Update settings.py

--- django_azure/settings.py
+++ django_azure/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@j%8$5lg#=6qyxxu*zb5i5dzehscu()&@kss8bu19q#m90*)&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
